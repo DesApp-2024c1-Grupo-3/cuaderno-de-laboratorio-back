fbadc6e57f8525650dc7b8b99e3485f4ee77d756
Agustin <agustin.fiordalisi@estudiantes.unahur.edu.ar>
2023-11-16 01:57:28 -0300

    se agrega endpoint para agregar un tp al curso de un profesor

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors"); // Importa el paquete CORS
+
 const app = express();
 const initDb = require("./config/db");
 const profesorRouter = require('./app/routes/profesor');
@@ -6,9 +8,10 @@ const alumnoRouter = require('./app/routes/alumno');
 const tpRouter = require('./app/routes/trabajopractico');
 const cursoRouter = require('./app/routes/curso');
 const grupoRouter = require('./app/routes/grupo');
-const materiaRouter = require('./app/routes/materia');
 
 const port = "8080";
+
+app.use(cors()); // Agrega el middleware CORS
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
@@ -16,12 +19,11 @@ app.listen(port, () => {
 
 app.use(express.json());
 
-
+// Usa el enrutador de Persona.js para manejar las rutas definidas en ese archivo
 app.use(profesorRouter);
 app.use(alumnoRouter);
 app.use(tpRouter);
 app.use(cursoRouter);
 app.use(grupoRouter);
-app.use(materiaRouter);
 
 initDb();
