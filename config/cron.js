7885e4837ce57c75ed1bd20c11e87215b757c9cb
ayortedra <cesaraugusto.pacheco@estudiantes.unahur.edu.ar>
2024-11-27 13:03:13 -0300

    Cron modificado para que actualice hora y dia de zona Argentina

--- config/cron.js
+++ config/cron.js
@@ -1,7 +1,11 @@
 const cron = require("node-cron");
 const controller = require("../app/controllers/trabajopractico");
 // Ejemplo de un cron job que corre cada minuto
+<<<<<<< HEAD
 cron.schedule('* * * * *', async () => {
+=======
+cron.schedule('*/1 * * * *', async () => {
+>>>>>>> b497123c00e17dcb3099bf0d20de3ececdccc6ab
   console.log(`Cron ejecutado a las: ${new Date().toISOString()}`);
   try {
     // Asegúrate de llamar a la función que no necesita req y res
